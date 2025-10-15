48797e943bbe4e8f21dcb5766f1cd708497ca628
Nilesh Shukla <shuklanilesh810@gmail.com>
2025-10-15 17:33:56 +0530

    chore: add ProjectDescription component to index to fix case mismatch

---
+++ projectPortfolio/src/components/ProjectDescription.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function ProjectDescription({projectName, projectDescription, workImg}) {
+  return (
+      <div className="p-4 bg-gray-300 rounded-2xl grid grid-cols-2 gap-4 w-[50vh] ">
+        <div className="flex items-center">
+          <img src={workImg} className="rounded-2xl my-auto" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl text-center font-oregano font-semibold">{projectName}</h1>
+            <p className="text-lg text-left">{projectDescription}</p>
+          </div>
+          <div className="w-full flex">
+            <button className="text-sm p-3 ml-auto bg-purple-600 hover:bg-purple-800 text-white w-fit rounded-xl cursor-pointer">Open Project</button>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default ProjectDescription
